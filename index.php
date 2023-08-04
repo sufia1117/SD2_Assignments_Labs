2cfaf575bd9d60972e177109cb8000b1dd25e1a4
sufia1117 <122894175+sufia1117@users.noreply.github.com>
2023-08-04 14:16:39 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    // Echo is used to display statements in PHP
+    echo"Display a message with PHP using echo <br>";
+    echo"Line breaks can be added within the quotes of an echo, written like in html. <br>";
+    echo"Comments are the same as in Java. HTML and CSS can be written in php files";
+    // So this is a comment in php 
+    /* This is a 
+        multi-line comment
+    */
+?>
+
